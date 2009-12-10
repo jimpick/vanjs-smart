b8a00bb32f84ed388a8a38c69c7886278eca2f98
Jim Pick <jim@jimpick.com>
2009-12-09 16:57:53 -0800

    Demo code.
    Can I make this look better?

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -5,3 +5,34 @@ GET("/", function() {
   return template("index.html");
 });
 
+var Name = new Resource('name');
+
+GET("/demo", function() {
+  var saved_names = Name.search({key: "demo"});
+  if (saved_names.length == 0) {  
+    this.saved_name = { name: "no name" };
+  } else {
+    this.saved_name = saved_names[0];
+  }
+  system.console.log("jim: " + uneval(this.saved_name));
+  return <form method="post">
+    Hello, <input type="text" name="name" value={this.saved_name.name} />. 
+    <input type="submit" />
+  </form>;
+});
+
+POST("/demo", function() {
+  var saved_names = Name.search({key: "demo"});
+  if (saved_names.length == 0) {  
+    this.saved_name = new Name();
+  } else {
+    this.saved_name = saved_names[0];
+  }
+  this.saved_name.key = "demo";
+  this.saved_name.name = this.request.body.name;
+  this.saved_name.save();
+  this.response.code = 201;
+  return redirect('/demo');
+});
+
+
